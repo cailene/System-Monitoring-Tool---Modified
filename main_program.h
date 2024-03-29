21ae7902a9b1c74836a2121202d2c98823716b39
cailene <cailene.magat@gmail.com>
2024-03-29 14:12:21 -0400

    finished mem util graphics

--- main_program.h
+++ main_program.h
@@ -2,7 +2,18 @@
 # include <stdlib.h>
 # include <stdio.h>
 
-# define CPUGRAPHICS "|"
+// CPU Graphics
+# define PERCPOS     "|"
+
+// Mem Graphics 
+# define NEG         "@"
+# define POS         "*"
+# define NEUT        "o"
+# define NEGPOINT    ":"
+# define POSPOINT    "#"
+
+# define ZERO 0
+
 
 # include "stats_functions.h"
 
@@ -13,4 +24,5 @@ int main(int argc, char ** argv);
 void printMemUtil(int iter, int samples, MemStruct *mem_usage);
 void printMemUtil(int iter, int samples, MemStruct *mem_usage);
 void printCPUInfoGraphics(int iter, int samples, CPUStruct *cpu_usage);
+void printMemUtilGraphics(int iter, int samples, MemStruct *mem_usage);
 #endif
