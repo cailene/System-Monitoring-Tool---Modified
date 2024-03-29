48969c0165554ee570f4cfa8111b01c668413eb4
cailene <cailene.magat@gmail.com>
2024-03-29 00:20:14 -0400

    add mem util fcns

--- stats_functions.c
+++ stats_functions.c
@@ -1,5 +1,57 @@
 # include "stats_functions.h"
 
+
+MemStruct initMemStruct(int samples){
+    MemStruct myStruct;
+    myStruct.mem_usage = (double **) malloc (sizeof(double) * samples);
+    if (myStruct.mem_usage == NULL){
+        fprintf(stderr, "Error allocating memory for cpu usage\n");
+        exit(EXIT_FAILURE);
+    }
+    for(int i = 0; i < samples; i++){
+        myStruct.mem_usage[i] = (double *) malloc (sizeof(double) * 4);
+        if (myStruct.mem_usage[i] == NULL) {
+            fprintf(stderr, "Error allocating memory for cpu usage\n");
+            exit(EXIT_FAILURE);
+        }
+        myStruct.mem_usage[i][MEMTOT] = 0.0;
+        myStruct.mem_usage[i][MEMUSED] = 0.0;
+        myStruct.mem_usage[i][MEMTOTVIRT] = 0.0;
+        myStruct.mem_usage[i][MEMUSEDVIRT] = 0.0;
+    }
+
+    return myStruct;
+}
+
+void getMemUsage(int iter, MemStruct *mem_usage){
+    double total_mem, used_mem, total_virt_mem, used_virt_mem;
+    struct sysinfo info;
+
+        if (sysinfo(&info) != 0) {
+            perror("Error getting system information");
+            exit(EXIT_FAILURE);
+        }
+
+    total_mem = (double)info.totalram * info.mem_unit / (1024.0 * 1024 * 1024);
+    used_mem = (double)(info.totalram - info.freeram) * info.mem_unit / (1024.0 * 1024 * 1024);
+
+    total_virt_mem = (double)(info.totalram + info.totalswap) * info.mem_unit / (1024 * 1024 * 1024);
+    used_virt_mem = (double)((info.totalram + info.totalswap) - (info.freeram - info.freeswap)) * info.mem_unit / (1024 * 1024 * 1024);
+    
+    mem_usage->mem_usage[iter][MEMTOT] = total_mem;
+    mem_usage->mem_usage[iter][MEMUSED] = used_mem;
+
+    mem_usage->mem_usage[iter][MEMTOTVIRT] = total_virt_mem;
+    mem_usage->mem_usage[iter][MEMUSEDVIRT] = used_virt_mem;
+}
+
+void deleteMem(int samples, MemStruct *mem_usage){
+    for (int i = 0; i < samples; i++) {
+        free(mem_usage->mem_usage[i]);
+    }
+    free(mem_usage->mem_usage);
+}
+
 CPUStruct initCPUStruct (int samples){
     // allocating memory to use CPUStruct
     // initializing CPUStruct
