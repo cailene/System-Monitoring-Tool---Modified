7b8be3868137821029f12cd43fa8b6b3f82d2a83
cailene <cailene.magat@gmail.com>
2024-03-29 23:18:51 -0400

    add makefile

---
+++ Makefile.mk
@@ -0,0 +1,17 @@
+CC = gcc
+CFLAGS = -Wall -g -std=c99 -Werror 
+
+stats: main_program.o stats_functions.o
+	$(CC) $(CFLAGS) -o $@ $^
+
+%.o: %.c main_program.h stats_functions.h
+	$(CC) $(CFLAGS) -c $<
+
+.PHONY: clean
+clean:
+	rm -f *.o stats
+.PHONY: help
+help:
+	@echo "stats:     Build the System Stats executable"
+	@echo "clean:     Remove auto-generated files"
+	@echo "help:      Display this help message"
