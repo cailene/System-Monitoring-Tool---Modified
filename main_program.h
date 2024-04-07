cf86e5603c8b34874970faedd6ddd4cc0be96ef9
cailene <cailene.magat@gmail.com>
2024-04-07 02:02:20 -0400

    wrote comments

--- main_program.h
+++ main_program.h
@@ -27,18 +27,40 @@
 #ifndef __Main_Program_header
 #define __Main_Program_header
 
+/* Handles reading command line arguments and information to print. 
+Implements pipes and forking to handle concurrent running of memory
+utilization, cpu utilization and getting user sessions.
+Also implements signals, specifically CTRL-Z and CTRL-C */
 int main(int argc, char ** argv);
 
+/* Signal functions*/
+/* If CTRL-Z is signalled, the program ignores this signal*/
 void ignore(int signum);
+/* If CTRL-C is signalled, the program asks the user if they want
+to terminate the program and handles approriately*/
 void leave(int signum);
 
-void printCPUInfo(int iter, int samples, CPUStruct *cpu_usage, SystemStats *stats);
-void printMemUtil(int iter, int samples, MemStruct *mem_usage);
-void printCPUInfoGraphics(int iter, int samples, CPUStruct *cpu_usage);
-void printMemUtilGraphics(int iter, int samples, MemStruct *mem_usage);
+/*Print functions*/
+/*Prints System information including:
+    System Name, 
+    Machine Name, 
+    Version, 
+    Release, 
+    Architecture, 
+    and Uptime */
 void printSysInfo(SystemStats *stats);
+/*Prints CPU utilization and the number of cores of your system */
+void printCPUInfo(int iter, int samples, CPUStruct *cpu_usage, SystemStats *stats);
+/*Prints memory utilization including physical and virtual memory */
+void printMemUtil(int iter, int samples, MemStruct *mem_usage);
+/*Prints the CPU utilization with graphics with PERCPOS sign*/
+void printCPUInfoGraphics(int iter, int samples, CPUStruct *cpu_usage);
+/*Prints the memory utilization with graphics with defined memory graphics*/
+void printMemUtilGraphics(int iter, int samples, MemStruct *mem_usage);
+
+/*Prints a message on how to use the program*/
 void Message();
 
-/*helper*/
+/*helper for printing graphics of cpu utilization*/
 int getdifference(double cur_mem, double pre_mem, int *difference);
 #endif
